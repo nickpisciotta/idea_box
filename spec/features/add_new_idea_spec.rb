8330b7cbbcc6cb55348bf78e6a3443200f7590e6
Nick Pisciotta <npisciotta@gmail.com>
2016-08-15 18:03:58 -0600

    Set up landing page

--- spec/features/add_new_idea_spec.rb
+++ spec/features/add_new_idea_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 RSpec.feature "Add a new Idea" do
   scenario "New Idea is Added" do
-
+    # before do
+    #   Capybara.current_driver = :webkit
+    # end
     visit root_path
 
+    fill_in "title", with: "New Idea"
+    fill_in "body", with: "This is a great idea!"
+    click_on "Save"
+
+    expect(page).to have_content("New Idea")
+    expect(Idea.count).to eq(1)
   end
 end
