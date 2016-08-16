aaba7c9360b0870ef611e0c0e10087294f726f17
Nick Pisciotta <npisciotta@gmail.com>
2016-08-15 22:28:19 -0600

    Create index action and endpoint with test

--- spec/requests/ideas_request_spec.rb
+++ spec/requests/ideas_request_spec.rb
@@ -18,4 +18,22 @@ describe "Ideas Controller" do
     expect(parsed_response["idea"]["body"]).to eq("Body1")
     expect(parsed_response["idea"]["quality"]).to eq("Swill")
   end
+
+  it "returns all ideas" do
+    idea = create_idea
+
+    expect(Idea.count).to eq(1)
+
+    get "/ideas"
+
+    expect(response.status).to eq(200)
+
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response.count).to eq(1)
+    expect(parsed_response["idea"].first["title"]).to eq("Idea1")
+    expect(parsed_response["idea"].first["body"]).to eq("Body1")
+    expect(parsed_response["idea"].first["quality"]).to eq("Swill")
+  end
+
 end
