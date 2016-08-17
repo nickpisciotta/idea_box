ceb0f78e7ce5f07782d867de05505c1b04fdf4f3
Nick Pisciotta <npisciotta@gmail.com>
2016-08-16 21:56:31 -0600

    Styling changes and working AJAX update call

--- app/assets/javascripts/idea_listeners.js
+++ app/assets/javascripts/idea_listeners.js
@@ -1,7 +1,7 @@
 function createIdeaListener(selector) {
   $(selector).on('click', function(){
-    var ideaTitle = $("#idea-title").val()
-    var ideaBody = $("#idea-body").val()
+    var ideaTitle = $("#title-box").val()
+    var ideaBody = $("#body-box").val()
     var postData = {idea: {
       title: ideaTitle,
       body: ideaBody
@@ -13,5 +13,11 @@ function createIdeaListener(selector) {
 function deleteIdeaListener(selector) {
   $("body").on('click', selector, function(){
   deleteIdea(this)
+  })
+}
+
+function editIdeaListener(selector) {
+  $("body").on("focusout", selector, function(){
+    editIdea(this)
   })
 }
